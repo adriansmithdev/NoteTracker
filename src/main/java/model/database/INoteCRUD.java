8d1150934b4f778ca4d877bb05e101bc2ea24680
Adrian Smith <ajsmith5796@gmail.com>
2018-10-22 09:26:38 -0700

    Added rest of CRUD operations

--- src/main/java/model/database/INoteCRUD.java
+++ src/main/java/model/database/INoteCRUD.java
@@ -4,5 +4,7 @@ import model.INote;
 
 public interface INoteCRUD {
     void addNote(INote note);
-
+    void removeNote(INote note);
+    void updateNote(INote note);
+    INote getNote(INote note);
 }

94ec3de902a026c5ccb1dc5389f4e02e3e71d690
Adrian Smith <ajsmith5796@gmail.com>
2018-10-22 10:50:28 -0700

    Cleaned up launcher

--- src/main/java/launch/Launcher.java
+++ src/main/java/launch/Launcher.java
@@ -8,8 +8,6 @@ import views.NoteTrackerUI;
 public class Launcher {
 
     public static void main(String[] args) {
-        NoteTrackerUI view = new NoteTrackerUI();
-        Controller controller = new Controller(view);
-        Application.launch(view.getClass(), args);
+        Application.launch(NoteTrackerUI.class, args);
     }
 }

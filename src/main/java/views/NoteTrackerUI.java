a9bc7b11a109c2b9f3026f9abe7c656a8dd1b74d
Adrian Smith <ajsmith5796@gmail.com>
2018-10-22 10:02:57 -0700

    Started Scene.

--- src/main/java/views/NoteTrackerUI.java
+++ src/main/java/views/NoteTrackerUI.java
@@ -1,13 +1,34 @@
 package views;
 
 import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class NoteTrackerUI extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    public static final int WIN_HEIGHT = 500;
+    public static final int WIN_WIDTH = 700;
+    private Stage stage;
 
+    @Override
+    public void start(Stage stage){
+        this.stage = stage;
+        stage.setScene(assembleScene());
+        stage.setTitle("NOTES");
+        stage.show();
     }
+
+    private Scene assembleScene()
+    {
+        VBox panel = new VBox();
+
+
+        return new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
+    }
+
 
 }

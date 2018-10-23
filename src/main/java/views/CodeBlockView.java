5dba6c8032cf8537cd9705176c33f7aa839099d9
Kyle <Kyl.johnson@gmail.com>
2018-10-22 20:18:25 -0700

    Started adding code block implementation and fixed compiler errors

--- src/main/java/views/CodeBlockView.java
+++ src/main/java/views/CodeBlockView.java
@@ -1,5 +1,38 @@
 package views;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class CodeBlockView implements IBaseNoteView {
 
+    String title;
+    String description;
+    String codeSegment;
+    String dateCreated;
+
+    public CodeBlockView(String title, String description, String codeSegment, String dateCreated) {
+        this.title = title;
+        this.description = description;
+        this.codeSegment = codeSegment;
+        this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public HBox getHBoxRepresentation() {
+        HBox panel = new HBox();
+        Label titleLabel = new Label(title);
+        Label date = new Label(dateCreated);
+
+        panel.getChildren().addAll(titleLabel, date);
+
+        return panel;
+    }
+
+    @Override
+    public VBox getExpandedInfo() {
+        VBox vBox = new VBox();
+
+        return null;
+    }
 }
--- src/main/java/views/ToDoView.java
+++ src/main/java/views/ToDoView.java
@@ -1,4 +1,16 @@
 package views;
 
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class ToDoView implements IBaseNoteView {
+    @Override
+    public HBox getHBoxRepresentation() {
+        return null;
+    }
+
+    @Override
+    public VBox getExpandedInfo() {
+        return null;
+    }
 }

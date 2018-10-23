cbd57f3d7380980fb3f23c58df9facfeb6240d33
Kyle <Kyl.johnson@gmail.com>
2018-10-23 09:49:32 -0700

    Pushing changes

--- src/main/java/views/CodeBlockView.java
+++ src/main/java/views/CodeBlockView.java
@@ -6,33 +6,13 @@ import javafx.scene.layout.VBox;
 
 public class CodeBlockView implements IBaseNoteView {
 
-    String title;
-    String description;
-    String codeSegment;
-    String dateCreated;
-
-    public CodeBlockView(String title, String description, String codeSegment, String dateCreated) {
-        this.title = title;
-        this.description = description;
-        this.codeSegment = codeSegment;
-        this.dateCreated = dateCreated;
-    }
-
     @Override
     public HBox getHBoxRepresentation() {
-        HBox panel = new HBox();
-        Label titleLabel = new Label(title);
-        Label date = new Label(dateCreated);
-
-        panel.getChildren().addAll(titleLabel, date);
-
-        return panel;
+        return null;
     }
 
     @Override
     public VBox getExpandedInfo() {
-        VBox vBox = new VBox();
-
         return null;
     }
 }

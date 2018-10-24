3589b87cf892c57c65ece851af6c7aa646c234ea
Kyle <Kyl.johnson@gmail.com>
2018-10-24 10:46:51 -0700

    Styles and note tracker ui

--- src/main/java/views/NoteTrackerUI.java
+++ src/main/java/views/NoteTrackerUI.java
@@ -2,19 +2,13 @@ package views;
 
 import controller.Controller;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import model.INote;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NoteTrackerUI extends Application {
 
@@ -22,6 +16,10 @@ public class NoteTrackerUI extends Application {
     public static final int WIN_WIDTH = 700;
     private Stage stage;
     private Controller controller = new Controller();
+    private static final String BUTTON_CLASS = "button-element";
+    private static final String HEADER_ID = "header";
+    private static final String FILTER_ID = "filter";
+    private static final String CREATE_ID = "create";
 
     @Override
     public void start(Stage stage){
@@ -36,19 +34,57 @@ public class NoteTrackerUI extends Application {
         VBox panel = new VBox();
 
         // add Notes
+        panel.getChildren().addAll(assembleMenuBar(), assembleCards());
 
-        return new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
+        Scene scene = new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
+        scene.getStylesheets().add(
+                getClass().getClassLoader()
+                        .getResource("NoteTrackerUI.css")
+                        .toExternalForm()
+        );
+
+        return scene;
     }
 
 
 
-    private HBox assembleMenuBar()
+    private BorderPane assembleMenuBar()
     {
-        HBox bar = new HBox();
-        Button createNoteBtn = new Button("+ Note");
+        BorderPane bar = new BorderPane();
+        Button createNote = new Button("+ Note");
+        Button filterNotes = new Button("Filter");
 
+        createNote.getStyleClass().add(BUTTON_CLASS);
+        filterNotes.getStyleClass().add(BUTTON_CLASS);
+        bar.setId(HEADER_ID);
+        filterNotes.setId(FILTER_ID);
+        createNote.setId(CREATE_ID);
 
+        bar.setLeft(createNote);
+        bar.setRight(filterNotes);
 
         return bar;
+    }
+
+    private TilePane assembleCards() {
+//        TilePane pane = new TilePane();
+//        pane.setPrefColumns(2);
+//        pane.setPrefWidth(8);
+//        pane.getChildren().addAll(
+//                new Label("TEST1"),
+//                new Label("TEST2"),
+//                new Label("TEST3"),
+//                new Label("TEST4"),
+//                new Label("TEST5")
+//        );
+
+        TilePane tile = new TilePane();
+        tile.setHgap(8);
+        tile.setPrefColumns(4);
+        for (int i = 0; i < 20; i++) {
+            tile.getChildren().add(new Label("TEST2"));
+        }
+
+        return tile;
     }
 }

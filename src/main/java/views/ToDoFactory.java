b6dafeed2318164561aff2773114b20fa6bd7523
Adrian Smith <ajsmith5796@gmail.com>
2018-11-05 11:34:05 -0800

    ToDoFactory and WebLinkFactory completed.

--- src/main/java/views/ToDoFactory.java
+++ src/main/java/views/ToDoFactory.java
@@ -1,17 +1,47 @@
 package views;
 
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.INote;
+import model.ToDo;
+import model.ToDoItem;
+
+import java.util.List;
 
 public class ToDoFactory implements INoteCreator  {
     @Override
     public HBox createSampleView(INote note) {
-        return null;
+        ToDo toDoList = (ToDo) note;
+        HBox toDoView = new HBox();
+
+        Label title = new Label(toDoList.getTitle() + " To-Do's");
+
+        toDoView.getChildren().addAll(title);
+
+        return toDoView;
     }
 
     @Override
     public VBox createExpandedView(INote note) {
-        return null;
+        ToDo toDoList = (ToDo) note;
+        VBox toDoView = new VBox();
+
+        Label title = new Label(toDoList.getTitle() + " To-Do's");
+        List<ToDoItem> listOfToDos = toDoList.getToDoItems();
+
+        CheckBox[] toDoBoxes = new CheckBox[listOfToDos.size()];
+
+        for(int i = 0; i < listOfToDos.size(); i++){
+            ToDoItem individualToDo = listOfToDos.get(i);
+            toDoBoxes[i] = new CheckBox(individualToDo.getToDo());
+            toDoBoxes[i].setSelected(individualToDo.isCompleted());
+        }
+
+        toDoView.getChildren().add(title);
+        toDoView.getChildren().addAll(toDoBoxes);
+
+        return toDoView;
     }
 }
--- src/main/java/views/WebLinkFactory.java
+++ src/main/java/views/WebLinkFactory.java
@@ -1,17 +1,35 @@
 package views;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import model.INote;
+import model.WebLink;
 
 public class WebLinkFactory implements INoteCreator  {
     @Override
     public HBox createSampleView(INote note) {
-        return null;
+        WebLink link = (WebLink) note;
+        HBox linkView = new HBox();
+
+        Label title = new Label(link.getTitle());
+
+        linkView.getChildren().addAll(title);
+
+        return linkView;
     }
 
     @Override
     public VBox createExpandedView(INote note) {
-        return null;
+        WebLink link = (WebLink) note;
+        VBox linkView = new VBox();
+
+        Label title = new Label(link.getTitle());
+        Text hyperlink = new Text(link.getURL());
+
+        linkView.getChildren().addAll(title, hyperlink);
+
+        return linkView;
     }
 }
